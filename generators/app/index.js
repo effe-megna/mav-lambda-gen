e5a35dbbdc603f2c26b0a7d2168c32fd08f025b5
Francesco Megna <francesco.megna97@gmail.com>
2020-01-19 21:22:42 +0100

    Revert "aggiunta dei template Cloudformation, definizione della Pipeline"

--- generators/app/index.js
+++ generators/app/index.js
@@ -12,21 +12,6 @@ module.exports = class extends Generator {
     const prompts = [
       {
         type: 'input',
-        name: 'newStackName',
-        message : 'Enter a name for the new AWS CloudFormation stack (i.e.: apiGetMavMembers): '
-      },
-      {
-        type: 'input',
-        name: 'newApiEndpoint',
-        message : 'Enter a name for the new API Gateway endpoint (i.e.: members): '
-      },
-      {
-        type: 'input',
-        name: 'newApiMethod',
-        message : 'Enter a name for the new API Gateway method (i.e.: GET): '
-      },
-      {
-        type: 'input',
         name: 'newLambdaName',
         message : 'Enter a name for the new lambda (i.e.: api_get_mav_members): '
       },
@@ -62,44 +47,14 @@ module.exports = class extends Generator {
         newAuthor: this.props.newAuthor
       }
     );
-    this.fs.copyTpl(
-      this.templatePath("template-development.json"),
-      this.destinationPath("template-development.json"),
-      {
-        newLambdaName: this.props.newLambdaName,
-        newDescription: this.props.newDescription,
-        newApiEndpoint: this.props.newApiEndpoint,
-        newApiMethod: this.props.newApiMethod
-      }
-    );
-    this.fs.copyTpl(
-      this.templatePath("template.json"),
-      this.destinationPath("template.json"),
-      {
-        newLambdaName: this.props.newLambdaName,
-        newDescription: this.props.newDescription,
-        newApiEndpoint: this.props.newApiEndpoint,
-        newApiMethod: this.props.newApiMethod
-      }
-    );
-    this.fs.copyTpl(
-      this.templatePath("bitbucket-pipelines.yml"),
-      this.destinationPath("bitbucket-pipelines.yml"),
-      {
-        newLambdaName: this.props.newLambdaName,
-        newStackName: this.props.newStackName
-      }
-    );
 
     smartCopy("tsconfig.json")
     smartCopy(".editorconfig")
     smartCopy("jest.config.js")
     smartCopy(".gitignore")
-    smartCopy(".nvmrc")
     smartCopy("src/index.ts")
     smartCopy("src/types.ts")
     smartCopy("src/logic.ts")
-    smartCopy(".vscode/launch.json")
   }
 
   install() {
