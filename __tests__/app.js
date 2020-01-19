82cdd227aef1a85a651a2fee2eb296b80efc0266
Claudio Bertozzi <claudio.bertozzi@mavigex.com>
2020-01-19 02:33:28 +0100

    - added files; - enable debug setup; - included stack templates; - included pipeline configuration; - improved/extended test;

--- __tests__/app.js
+++ __tests__/app.js
@@ -1,17 +1,20 @@
 "use strict";
 
 const { join } = require('path');
-const { file, JSONFileContent } = require('yeoman-assert');
+const { file, JSONFileContent, fileContent } = require('yeoman-assert');
 const { run } = require('yeoman-test');
 
 describe('mav-lambda-gen:app', () => {
   beforeAll(() => {
     return run(join(__dirname, '../generators/app'))
-      .withPrompts(
-        { newLambdaName: "api_get_mav_members" },
-        { newDescription: "provide mav members to clients" },
-        { newAuthor: "Mavigex" }
-      );
+      .withPrompts({
+        newStackName: "apiGetMavMembers",
+        newApiEndpoint: "members",
+        newApiMethod: "GET",
+        newLambdaName: "api_get_mav_members",
+        newDescription: "provide mav members to clients",
+        newAuthor: "Mavigex"
+       });
   });
 
   it('package.json was created', () => {
@@ -19,11 +22,22 @@ describe('mav-lambda-gen:app', () => {
   });
 
   it('package.json was created based on answers', () => {
-    JSONFileContent("package.json", { 
+    JSONFileContent("package.json", {
       name: "api_get_mav_members",
-      // description: "provide mav members to clients", ** strange behavior
+      description: "provide mav members to clients",
       author: "Mavigex"
     })
+  });
+
+  it('package.json was created with SAM debug and invoke scripts', () => {
+    JSONFileContent("package.json", {
+      "scripts": {
+        "predebug": "tsc; rsync -qrk --exclude='.git' node_modules dist",
+        "debug": "sam local invoke -t template-development.json -d 9229",
+        "preinvoke": "tsc; rsync -qrk --exclude='.git' node_modules dist",
+        "invoke": "sam local invoke -t template-development.json"
+      }
+    });
   });
 
   it('tsconfig.json was created', () => {
@@ -42,6 +56,12 @@ describe('mav-lambda-gen:app', () => {
     file(['.gitignore']);
   });
 
+  it('.nvmrc was created', () => {
+    file(['.nvmrc']);
+    fileContent('.nvmrc', 'v12');
+  });
+
+
   it('src/index.ts was created', () => {
     file(['src/index.ts']);
   });
@@ -52,5 +72,91 @@ describe('mav-lambda-gen:app', () => {
 
   it('src/types.ts was created', () => {
     file(['src/types.ts']);
+  });
+
+  it('VS Code debug setup was created', () => {
+    file(['.vscode/launch.json']);
+  });
+
+  it('CloudFormation template for development stage was created', () => {
+    file(['template-development.json']);
+  });
+
+  it('CloudFormation template for development stage was created based on answers', () => {
+    JSONFileContent("template-development.json", {
+      "Resources": {
+        "apiGatewayResource": {
+          "Type" : "AWS::ApiGateway::Resource",
+          "Properties" : {
+              "ParentId" : {"Ref": "ApiRootResourceId"},
+              "PathPart" : "members",
+              "RestApiId" : {"Ref": "ApiId"}
+          }
+        },
+        "resourceMethod": {
+          "Type" : "AWS::ApiGateway::Method",
+          "Properties" : {
+              "HttpMethod" : "GET"
+          }
+        },
+        "lambdaFunction": {
+          "Properties": {
+            "FunctionName": "api_get_mav_members"
+          }
+        },
+        "lambdaFunctionAliasDevelopmentInvokePermission": {
+          "Type": "AWS::Lambda::Permission",
+          "Properties": {
+              "SourceArn": {"Fn::Sub": "arn:aws:execute-api:${AWS::Region}:${AWS::AccountId}:${ApiId}/*/GET/members"}
+          }
+        }
+      }
+    });
+  });
+
+  it('CloudFormation template was created', () => {
+    file(['template.json']);
+  });
+
+  it('CloudFormation template was created based on answers', () => {
+    JSONFileContent("template.json", {
+      "Resources": {
+        "apiGatewayResource": {
+          "Type" : "AWS::ApiGateway::Resource",
+          "Properties" : {
+              "ParentId" : {"Ref": "ApiRootResourceId"},
+              "PathPart" : "members",
+              "RestApiId" : {"Ref": "ApiId"}
+          }
+        },
+        "resourceMethod": {
+          "Type" : "AWS::ApiGateway::Method",
+          "Properties" : {
+              "HttpMethod" : "GET"
+          }
+        },
+        "lambdaFunction": {
+          "Properties": {
+            "FunctionName": "api_get_mav_members"
+          }
+        },
+        "lambdaFunctionAliasStagingInvokePermission": {
+          "Type": "AWS::Lambda::Permission",
+          "Properties": {
+              "SourceArn": {"Fn::Sub": "arn:aws:execute-api:${AWS::Region}:${AWS::AccountId}:${ApiId}/*/GET/members"}
+          }
+        },
+        "lambdaFunctionAliasProductionInvokePermission": {
+          "Type": "AWS::Lambda::Permission",
+          "Properties": {
+              "SourceArn": {"Fn::Sub": "arn:aws:execute-api:${AWS::Region}:${AWS::AccountId}:${ApiId}/*/GET/members"}
+          }
+        }
+      }
+    });
+  });
+
+  it('Bitbucket pipeline description file was created', () => {
+    file(['bitbucket-pipelines.yml']);
   });
 });
